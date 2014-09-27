9d52bf8103db31eef68ef66361df6a22cdc7a65f
Chris Boon <cboon27@gmail.com>
2014-09-27 03:01:47 -0400

    Another random commit

---
+++ scripts/sanguily.js
@@ -0,0 +1,8 @@
+// Dependencies: jQuery (using: 1.11.1)
+$(".section--branding__grid-item").on('click',function(){
+  $(".content-viewer").removeClass("content-viewer--empty");
+});
+
+$(".content-viewer__close").on('click',function(){
+  $(".content-viewer").addClass("content-viewer--empty");
+});
